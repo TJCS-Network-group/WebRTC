9beadc662bf91388ae814bb917b08f4bb7d105fa
gzy_02 <1262454489@qq.com>
2022-06-25 06:43:57 +0800

    fake data

--- api/login_routes.py
+++ api/login_routes.py
@@ -5,20 +5,24 @@ from api import api_blue
 from werkzeug.security import generate_password_hash
 from flask import request
 
+
 def check_and_login(stu_no, password=None):
-    need=[Student.id,Student.stu_grade,Student.stu_password,Student.stu_enable,Student.stu_userlevel]
+    need = [
+        Student.id, Student.stu_grade, Student.stu_password,
+        Student.stu_enable, Student.stu_userlevel
+    ]
     users = Student.select(*need).where(Student.stu_no == stu_no)
-    if users.count()==0:#没找到
+    if users.count() == 0:  #没找到
         return make_response_json(400, "账号不存在")
-    elif users.count()>1:#有多个，则以grade最大的为准（考虑重修）
-        user=None
-        user_grade=-1
+    elif users.count() > 1:  #有多个，则以grade最大的为准（考虑重修）
+        user = None
+        user_grade = -1
         for tepuser in users:
-            if user_grade<tepuser.stu_grade:
-                user=tepuser
-                user_grade=tepuser.stu_grade
-    if users.count()==1:#仅仅一个
-        user=users[0]
+            if user_grade < tepuser.stu_grade:
+                user = tepuser
+                user_grade = tepuser.stu_grade
+    if users.count() == 1:  #仅仅一个
+        user = users[0]
     if password != None:
         if not user.check_password(password):
             return make_response_json(400, "密码错误")
@@ -31,26 +35,31 @@ def check_and_login(stu_no, password=None):
     login_user(user, True, timedelta(days=30))
     print(user.stu_userlevel)
     if user.check_password(stu_no):
-        return make_response_json(data={"url":"/ChangePassword"})
-    if user.stu_userlevel==User_level.Normal.value:
-        target_url="/Video"
-    elif user.stu_userlevel==User_level.Admin.value:
-        target_url="/Admin"
+        return make_response_json(data={"url": "/ChangePassword"})
+    if user.stu_userlevel == User_level.Normal.value:
+        target_url = "/Video"
+    elif user.stu_userlevel == User_level.Admin.value:
+        target_url = "/Admin"
     resp = make_response_json(data={"url": target_url})
     resp.set_cookie("account", stu_no)
     return resp
 
-def check_password_pattern(password:str) -> bool:
+
+def check_password_pattern(password: str) -> bool:
     num = re.compile("[0-9]")
     small_letter = re.compile("[a-z]")
     big_letter = re.compile("[A-Z]")
     special = re.compile("[+\-\*_&%]")
     # s = [reps.findall(password) for reps in [num,small_letter,big_letter,special]]
-    result = [len(reps.findall(password)) for reps in [num,small_letter,big_letter,special]]
+    result = [
+        len(reps.findall(password))
+        for reps in [num, small_letter, big_letter, special]
+    ]
     # print(s,result)
     if 0 in result or sum(result) != len(password):
         return False
     return True
+
 
 @api_blue.route('/login_using_password', methods=['POST'])
 def login_using_password():
@@ -59,6 +68,7 @@ def login_using_password():
     session['account'] = str(user_no)
     return check_and_login(user_no, password)
 
+
 @api_blue.route('/logout')
 def logout():
     logout_user()
@@ -66,50 +76,57 @@ def logout():
     resp.delete_cookie("account")
     return resp
 
-@api_blue.route("/myinfo",methods=["GET"])
+
+@api_blue.route("/myinfo", methods=["GET"])
 def myinfo():
     if not session.get('account'):
-        return make_response_json(400,"请先登录",data={"url":"/Login"})
+        return make_response_json(400, "请先登录", data={"url": "/Login"})
     if not current_user.is_authenticated:
-        return make_response_json(400,"请先登录",data={"url":"/Login"})
-    return make_response_json(200,"信息",data={"stu_no":current_user.stu_no,"stu_name":current_user.stu_name})
-    
+        return make_response_json(400, "请先登录", data={"url": "/Login"})
+    return make_response_json(200,
+                              "信息",
+                              data={
+                                  "stu_no": current_user.stu_no,
+                                  "stu_name": current_user.stu_name
+                              })
+
 
 @api_blue.route('/get_identity', methods=['GET'])
 def get_identity():
-    retData={}
+    retData = {}
     if not session.get('account'):
-        retData['isLogin']=False
-        return make_response_json(200,"",data=retData)
+        retData['isLogin'] = False
+        return make_response_json(200, "", data=retData)
     if not current_user.is_authenticated:
-        retData['isLogin']=False
-        return make_response_json(200,"",data=retData)
+        retData['isLogin'] = False
+        return make_response_json(200, "", data=retData)
     else:
-        retData['isLogin']=True
+        retData['isLogin'] = True
     if current_user.stu_userlevel == User_level.Admin.value:
-        retData['isAdmin']=True
+        retData['isAdmin'] = True
     else:
-        retData['isAdmin']=False
-    return make_response_json(200,"",data=retData)
+        retData['isAdmin'] = False
+    return make_response_json(200, "", data=retData)
 
 
-@api_blue.route("/change_password",methods=["POST"])
+from hashlib import md5
+
+
+@api_blue.route("/change_password", methods=["POST"])
 def change_password():
     if not current_user.is_authenticated:
-        return make_response_json(400,"请先登录",data={"url":"/Login"})
+        return make_response_json(400, "请先登录", data={"url": "/Login"})
     try:
         passwd = request.form.get('password')
     except Exception as e:
-        return make_response_json(400,"无新密码")
+        return make_response_json(400, "无新密码")
     if current_user.check_password(passwd):
-        return make_response_json(400,"新密码不能与原密码一致")
+        return make_response_json(400, "新密码不能与原密码一致")
     if not check_password_pattern(passwd):
-        return make_response_json(400,"新密码不符合网站标准")
-    current_user.stu_password =generate_password_hash(password=passwd,
-                                                       method="pbkdf2:md5")
+        return make_response_json(400, "新密码不符合网站标准")
+    current_user.stu_password = md5(passwd.encode("utf-8")).hexdigest()
     current_user.save()
     if current_user.stu_userlevel == User_level.Admin.value:
-        return make_response_json(200,"修改成功",data={"url":"/Admin"})
+        return make_response_json(200, "修改成功", data={"url": "/Admin"})
     else:
-        return make_response_json(200,"修改成功",data={"url":"/Video"})
-
+        return make_response_json(200, "修改成功", data={"url": "/Video"})
--- app/init_database.py
+++ app/init_database.py
@@ -14,56 +14,13 @@ def create_tables():
     Student.create_table()
 
 
-from werkzeug.security import generate_password_hash
-
+from hashlib import md5
 
 def fake_data():  #填一些假数据进去
-    """
     Student.create(stu_grade="2019",
                    stu_no="1234567",
                    stu_name="张三",
-                   stu_password=generate_password_hash(password="1234567",
-                                                       method="pbkdf2:md5"),
-                   stu_sex="男",
-                   stu_class_fname="计算机科学与技术",
-                   stu_class_sname="计科",
-                   stu_term="2021/2022/2",
-                   stu_cno="10106203",
-                   stu_wtype=0,
-                   stu_userlevel=User_level.Normal.value,
-                   stu_enable=True)
-    
-    Student.create(stu_grade="2019",
-                   stu_no="1950638",
-                   stu_name="陈冠忠",
-                   stu_password=generate_password_hash(password="1950638",
-                                                       method="pbkdf2:md5"),
-                   stu_sex="男",
-                   stu_class_fname="计算机科学与技术",
-                   stu_class_sname="计科",
-                   stu_term="2021/2022/2",
-                   stu_cno="10106203",
-                   stu_wtype=0,
-                   stu_userlevel=User_level.Normal.value,
-                   stu_enable=True)
-    Student.create(stu_grade="2019",
-                   stu_no="1951705",
-                   stu_name="高曾谊",
-                   stu_password=generate_password_hash(password="1951705",
-                                                       method="pbkdf2:md5"),
-                   stu_sex="男",
-                   stu_class_fname="计算机科学与技术",
-                   stu_class_sname="计科",
-                   stu_term="2021/2022/2",
-                   stu_cno="10106203",
-                   stu_wtype=0,
-                   stu_userlevel=User_level.Normal.value,
-                   stu_enable=True)
-    Student.create(stu_grade="2019",
-                   stu_no="1953493",
-                   stu_name="程森",
-                   stu_password=generate_password_hash(password="1953493",
-                                                       method="pbkdf2:md5"),
+                   stu_password=md5("1234567".encode("utf-8")).hexdigest(),
                    stu_sex="男",
                    stu_class_fname="计算机科学与技术",
                    stu_class_sname="计科",
@@ -75,8 +32,7 @@ def fake_data():  #填一些假数据进去
     Student.create(stu_grade="0000",
                    stu_no="9999999",
                    stu_name="监控",
-                   stu_password=generate_password_hash(password="9999999",
-                                                       method="pbkdf2:md5"),
+                   stu_password=md5("9999999".encode("utf-8")).hexdigest(),
                    stu_sex="男",
                    stu_class_fname="计算机",
                    stu_class_sname="计算机",
@@ -85,8 +41,30 @@ def fake_data():  #填一些假数据进去
                    stu_wtype=0,
                    stu_userlevel=User_level.Admin.value,
                    stu_enable=True)
-    """
-
+    Student.create(stu_grade="0000",
+                   stu_no="8888888",
+                   stu_name="监控",
+                   stu_password=md5("8888888".encode("utf-8")).hexdigest(),
+                   stu_sex="男",
+                   stu_class_fname="计算机",
+                   stu_class_sname="计算机",
+                   stu_term="0000",
+                   stu_cno="0000",
+                   stu_wtype=0,
+                   stu_userlevel=User_level.Admin.value,
+                   stu_enable=True)
+    Student.create(stu_grade="0000",
+                   stu_no="7777777",
+                   stu_name="监控",
+                   stu_password=md5("7777777".encode("utf-8")).hexdigest(),
+                   stu_sex="男",
+                   stu_class_fname="计算机",
+                   stu_class_sname="计算机",
+                   stu_term="0000",
+                   stu_cno="0000",
+                   stu_wtype=0,
+                   stu_userlevel=User_level.Admin.value,
+                   stu_enable=True)
     Student.create(id=100,
                    stu_grade="2019",
                    stu_no="1750819",
